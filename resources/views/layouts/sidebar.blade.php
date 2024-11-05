8c5d1483af4d0f66a30d5dbb3d8575dd93b7528c
usernameAnda <aurasagita2122gmail.com>
2024-11-05 08:38:03 +0700

    Template_Commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,128 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="{{ url('/') }}" class="brand-link">
+        <img src="https://vemto.app/favicon.png" alt="Vemto Logo" class="brand-image bg-white img-circle">
+        <span class="brand-text font-weight-light">Pemeliharaan Telemetri [Imported]</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column nav-child-indent" data-widget="treeview" role="menu">
+
+                @auth
+                <li class="nav-item">
+                    <a href="{{ route('home') }}" class="nav-link">
+                        <i class="nav-icon icon ion-md-pulse"></i>
+                        <p>
+                            Dashboard
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon icon ion-md-apps"></i>
+                        <p>
+                            Apps
+                            <i class="nav-icon right icon ion-md-arrow-round-back"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                            @can('view-any', App\Models\User::class)
+                            <li class="nav-item">
+                                <a href="{{ route('users.index') }}" class="nav-link">
+                                    <i class="nav-icon icon ion-md-radio-button-off"></i>
+                                    <p>Users</p>
+                                </a>
+                            </li>
+                            @endcan
+                            @can('view-any', App\Models\Pemeriksaan::class)
+                            <li class="nav-item">
+                                <a href="{{ route('pemeriksaans.index') }}" class="nav-link">
+                                    <i class="nav-icon icon ion-md-radio-button-off"></i>
+                                    <p>Pemeriksaans</p>
+                                </a>
+                            </li>
+                            @endcan
+                            @can('view-any', App\Models\Pemeliharaan::class)
+                            <li class="nav-item">
+                                <a href="{{ route('pemeliharaans.index') }}" class="nav-link">
+                                    <i class="nav-icon icon ion-md-radio-button-off"></i>
+                                    <p>Pemeliharaans</p>
+                                </a>
+                            </li>
+                            @endcan
+                            @can('view-any', App\Models\JenisPeralatan::class)
+                            <li class="nav-item">
+                                <a href="{{ route('jenis-peralatans.index') }}" class="nav-link">
+                                    <i class="nav-icon icon ion-md-radio-button-off"></i>
+                                    <p>Jenis Peralatans</p>
+                                </a>
+                            </li>
+                            @endcan
+                            @can('view-any', App\Models\PeralatanTelemetri::class)
+                            <li class="nav-item">
+                                <a href="{{ route('peralatan-telemetris.index') }}" class="nav-link">
+                                    <i class="nav-icon icon ion-md-radio-button-off"></i>
+                                    <p>Peralatan Telemetris</p>
+                                </a>
+                            </li>
+                            @endcan
+                            @can('view-any', App\Models\Komponen::class)
+                            <li class="nav-item">
+                                <a href="{{ route('komponens.index') }}" class="nav-link">
+                                    <i class="nav-icon icon ion-md-radio-button-off"></i>
+                                    <p>Komponens</p>
+                                </a>
+                            </li>
+                            @endcan
+                            @can('view-any', App\Models\Setting::class)
+                            <li class="nav-item">
+                                <a href="{{ route('settings.index') }}" class="nav-link">
+                                    <i class="nav-icon icon ion-md-radio-button-off"></i>
+                                    <p>Settings</p>
+                                </a>
+                            </li>
+                            @endcan
+                            @can('view-any', App\Models\Pemeliharaan::class)
+                            <li class="nav-item">
+                                <a href="{{ route('pemeliharaans.index') }}" class="nav-link">
+                                    <i class="nav-icon icon ion-md-radio-button-off"></i>
+                                    <p>Forms</p>
+                                </a>
+                            </li>
+                            @endcan
+                    </ul>
+                </li>
+
+                @endauth
+
+                <li class="nav-item">
+                    <a href="https://adminlte.io/docs/3.1//index.html" target="_blank" class="nav-link">
+                        <i class="nav-icon icon ion-md-help-circle-outline"></i>
+                        <p>Docs</p>
+                    </a>
+                </li>
+
+                @auth
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="nav-icon icon ion-md-exit"></i>
+                        <p>{{ __('Logout') }}</p>
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
+                @endauth
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
