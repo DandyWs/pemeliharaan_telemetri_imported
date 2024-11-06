1258a19345ec8a9b0511a76d07e030af86aabff0
serliputri <serliputrimaharani@gmail.com>
2024-11-06 13:02:29 +0700

    form mekanik

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,7 +1,9 @@
-<nav class="navbar navbar-expand navbar-light navbar-white">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm p-2">
+    <div class="container">
+        
+        <a class="navbar-brand text-primary font-weight-bold text-uppercase" href="{{ url('/') }}">
+            Jasa Tirta I
+        </a>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <!-- Left Side Of Navbar -->
