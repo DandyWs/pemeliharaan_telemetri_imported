dc7cf2bd81374059d0a43eae47fdb70ef35b4d0f
DandyWs <Dandyws71@gmail.com>
2024-11-06 09:31:37 +0700

    Auth

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
-use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SettingController;
 use App\Http\Controllers\KomponenController;
