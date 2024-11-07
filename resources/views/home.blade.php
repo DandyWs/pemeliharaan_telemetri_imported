9e39fc97a486a21f2a7b569e06ffbbab41b47e23
usernameAnda <aurasagita2122gmail.com>
2024-11-07 15:41:42 +0700

    fix_icon_dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,13 +12,12 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    
+
                     Selamat datang di aplikasi Form Pemeliharaan Jasa Tirta I, selamat bekerja
                     {{ Auth::user()->name }}
-
                 </div>
             </div>
-        </div>           
+        </div>
             <div class="row">
                 <div class="col-md-12 grid-margin">
                     @if (session('message'))
@@ -33,89 +32,88 @@
                           <b><h1>Dashboard {{ Auth::user()->role }}</h1></b>
                         </div>
                       </div>
-        
+
                         <div class="card-header border-0">
                             <!-- Main content -->
                             <div class="card">
                                 <div class="card-header">
-                                  <h3 class="card-title"><i class="nav-icon fas fa-home"></i> Dashboard</h3>
+                                  <h3 class="card-title"><i class="nav-icon icon ion-md-pulse"></i> Dashboard</h3>
                                 </div>
                                 <div class="card-body">
                         <section class="content">
-        
-                        <!-- Default box -->
-                        <div class="container-fluid">
-                            <!-- Small boxes (Stat box) -->
-                            <div class="row">
-                              <div class="col-lg-4 col-6">
-                                <!-- small box -->
-                                @if (Auth::user()->role == 'admin')
-                                <div class="small-box bg-info">
-                                  <div class="inner">
-                                    <h3>{{ $countUser }}</h3>
-                                    <p>User</p>
-                                  </div>
-                                  <div class="icon">
-                                    <i class="fas fa-users fa-2x"></i>
-                                  </div>
-                                  <a href="{{ url('/users') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                                </div>
-                                @else
-                                <div class="small-box bg-info">
-                                  <div class="inner">
-                                    <h3>{{ $countPemeriksaan }}</h3>
-                                    <p>Form Pemeriksaan</p>
-                                  </div>
-                                  <div class="icon">
-                                    <i class="fa fa-book"></i>
-                                  </div>
-                                  <a href="{{ url('/pemeriksaans') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                                </div>
-                                @endif
-                              </div>
-                              <!-- ./col -->
-                              <div class="col-lg-4 col-6">
-                                <!-- small box -->
-                                <div class="small-box bg-warning">
-                                  <div class="inner">
-                                    <h3>{{ $countPemeliharaan }}</h3>
-                                    <p>Form Pemeliharaan</p>
-                                  </div>
-                                  <div class="icon">
-                                    <i class="fa fa-book"></i>
-                                  </div>
-                                  <a href="{{ url('/pemeliharaans') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                                </div>
-                              </div>
-                              <!-- ./col -->
-                              <div class="col-lg-4 col-6">
-                                <!-- small box -->
-                                <div class="small-box bg-danger">
-                                  <div class="inner">
-                                    <h3>{{ $countPeralatanTelemetri }}</h3>
-                    
-                                    <p>Jumlah Alat</p>
-                                  </div>
-                                  <div class="icon">
-                                    <i class="fa fa-calendar"></i>
-                                  </div>
-                                  <a href="{{ url('/peralatan-telemetris') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                                </div>
-                              </div>
-                              <!-- ./col -->
+
+                                <!-- Default box -->
+                                <div class="container-fluid">
+                                    <!-- Small boxes (Stat box) -->
+                                    <div class="row">
+                                        <div class="col-lg-4 col-6">
+                                            <!-- small box -->
+                                            @if (Auth::user()->role == 'admin')
+                                            <div class="small-box bg-info">
+                                                <div class="inner">
+                                                    <h3>{{ $countUser }}</h3>
+                                                    <p>User</p>
+                                                </div>
+                                                <div class="icon d-flex align-items-center justify-content-center">
+                                                    <i class="nav-icon icon ion-md-users" style="font-size: 2.5rem;"></i>
+                                                </div>
+                                                <a href="{{ url('/users') }}" class="small-box-footer">More info <i class="nav-icon icon ion-md-arrow-forward" style="font-size: 1.5rem;"></i></a>
+                                            </div>
+                                            @else
+                                            <div class="small-box bg-info">
+                                                <div class="inner">
+                                                    <h3>{{ $countPemeriksaan }}</h3>
+                                                    <p>Form Pemeriksaan</p>
+                                                </div>
+                                                <div class="icon d-flex align-items-center justify-content-center">
+                                                    <i class="nav-icon icon ion-md-document" style="font-size: 2.5rem;"></i>
+                                                </div>
+                                                <a href="{{ url('/pemeriksaans') }}" class="small-box-footer">More info <i class="nav-icon icon ion-md-arrow-forward" style="font-size: 1.5rem;"></i></a>
+                                            </div>
+                                            @endif
+                                        </div>
+                                        <!-- ./col -->
+                                        <div class="col-lg-4 col-6">
+                                            <!-- small box -->
+                                            <div class="small-box bg-warning">
+                                                <div class="inner">
+                                                    <h3>{{ $countPemeliharaan }}</h3>
+                                                    <p>Form Pemeliharaan</p>
+                                                </div>
+                                                <div class="icon d-flex align-items-center justify-content-center">
+                                                    <i class="nav-icon icon ion-md-create" style="font-size: 2.5rem;"></i>
+                                                </div>
+                                                <a href="{{ url('/pemeliharaans') }}" class="small-box-footer">More info <i class="nav-icon icon ion-md-arrow-forward" style="font-size: 1.5rem;"></i></a>
+                                            </div>
+                                        </div>
+                                        <!-- ./col -->
+                                        <div class="col-lg-4 col-6">
+                                            <!-- small box -->
+                                            <div class="small-box bg-danger">
+                                                <div class="inner">
+                                                    <h3>{{ $countPeralatanTelemetri }}</h3>
+                                                    <p>Jumlah Alat</p>
+                                                </div>
+                                                <div class="icon d-flex align-items-center justify-content-center">
+                                                    <i class="nav-icon icon ion-md-calendar" style="font-size: 2.5rem;"></i>
+                                                </div>
+                                                <a href="{{ url('/peralatan-telemetris') }}" class="small-box-footer">More info <i class="nav-icon icon ion-md-arrow-forward" style="font-size: 1.5rem;"></i></a>
+                                            </div>
+                                        </div>
+                                        <!-- ./col -->
                             </div>
                             <div class="card">
                                 <div class="card-header">
                                     <b>Data Form Pemeliharaan</b>
-                                </div> 
+                                </div>
                                 <div class="row d-flex justify-between" style="width: 100%; justify-content: space-between; align-items: center; margin: 0">
                                     <form class="form" method="GET" action="{{ url('transaksi') }}" class="col-md-4" style="padding: 0">
                                       <div class="form-group w-100 mb-3">
                                       </div>
                                     </form>
-        
+
                                     <div class="card-body">
-                                     
+
                                         <table class="table table-bordered table-striped">
                                           <thead>
                                             <tr>
@@ -151,8 +149,8 @@
                                         </table>
                                       </div>
                                     </div>
-                                </div>                   
-        @endsection     
+                                </div>
+        @endsection
     </div>
-</div>                 
+</div>
 @endsection
