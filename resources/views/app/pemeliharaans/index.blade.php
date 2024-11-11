4301e64d6316a6b073aa922bd25099aef5cf0cf3
usernameAnda <aurasagita2122gmail.com>
2024-11-11 10:22:24 +0700

    fixing_table

--- resources/views/app/pemeliharaans/index.blade.php
+++ resources/views/app/pemeliharaans/index.blade.php
@@ -44,7 +44,7 @@
             </div>
 
             <div class="table-responsive">
-                <table class="table table-borderless table-hover">
+                <table class="table table-bordered table-striped">
                     <thead>
                         <tr>
                             <th class="text-left">
--- resources/views/app/users/index.blade.php
+++ resources/views/app/users/index.blade.php
@@ -41,7 +41,7 @@
             </div>
 
             <div class="table-responsive">
-                <table class="table table-borderless table-hover">
+                <table class="table table-bordered table-striped">
                     <thead>
                         <tr>
                             <th class="text-left">
